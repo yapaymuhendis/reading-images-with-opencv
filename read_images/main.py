a98f5e9c91fdac5428ecaa64a9fa10db3d43e429
cagan <109431950+yapaymuhendis@users.noreply.github.com>
2022-10-01 16:06:16 +0300

    Update main.py

--- read_images/main.py
+++ read_images/main.py
@@ -1,14 +1,12 @@
 import cv2
 
+# We use the imread command to read the picture to where the picture is.
 path = cv2.imread("images/test.PNG")
 
-
+#We use the imshow command to show the file it is reading.
 cv2.imshow("test", path)
 
-
+#If we do not use the waitkey command, the image will not stay on the screen.
 cv2.waitKey(0)
-cv2.destroyAllWindows() # herhangi bi tuşa basıldığında programı kapatıyor.
 
-
-
-
+cv2.destroyAllWindows() 
